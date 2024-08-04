efec791a20021441d1c12fcd07d7a21171a47fe4
Joonyoung Sung <sungso376@gmail.com>
2024-08-04 15:30:37 +0900

    20240804

---
+++ 14645.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int M, N, x, y;
+	cin >> M >> N;
+	while (M--)cin >> x >> y;
+	cout << "ºñ¿ÍÀÌ";
+}
