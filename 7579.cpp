524520561f2a5d9714ae60941802ccc07110e871
Joonyoung Sung Desktop <sungso376@gmail.com>
2024-08-16 20:42:34 +0900

    20240816

--- 7579.cpp
+++ 7579.cpp
@@ -2,28 +2,31 @@
 #include<queue>
 #include<utility>
 using namespace std;
-const int INF = 10000005;
-long long map[101][100001];
-int me[101];
-int cost[101];
+long long map[101][10001];
+long long me[101];
+long long cost[101];
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	int N, M;
+	long long N, M;
 	cin >> N >> M;
-	for (int i = 1; i <= N; i++)cin>>me[i];
-	for (int i = 1; i <= N; i++)cin>>cost[i];
+	for (int i = 1; i <= N; i++)cin >> me[i];
+	for (int i = 1; i <= N; i++)cin >> cost[i];
 	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j < 10001; j++) {
+		for (int j = 0; j <= 10001; j++) {
 			if (j - cost[i] >= 0) {
-				map[i][j] = (max(map[i - 1][j - cost[i]] + me[i], map[i - 1][j])) % (M + 1);
+				if (map[i - 1][j - cost[i]] + me[i] > map[i - 1][j]) {
+					long long tmp = (map[i - 1][j - cost[i]] + me[i]);
+					map[i][j] = tmp;
+				}
+				else map[i][j] = map[i - 1][j];
 			}
-			else map[i][j] = (map[i - 1][j])%(M+1);
+			else map[i][j] = map[i - 1][j];
 		}
 	}
-	for (int i = 0; i < 10001; i++){
-		
+	for (int i = 0; i <= 10001; i++) {
+		//cout << map[N][i] << " ";
 		if (map[N][i] >= M) {
 			cout << i;
 			break;
