be2ead9efc040d0deb27934976d7689758fd6c3c
Joonyoung Sung Desktop <sungso376@gmail.com>
2024-08-25 00:56:40 +0900

    20240825

--- 2743.cpp
+++ 2743.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	string a;
-	getline(cin, a);
-	cout << a.size();
+	string str;
+	cin >> str;
+	cout << str.length();
 }
