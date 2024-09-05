6801c2f6bf69fbff157a1c0c2ca63011170ff232
Joonyoung Sung Desktop <sungso376@gmail.com>
2024-09-05 18:28:53 +0900

    20240905

---
+++ 15650.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> v;
+void NM(int before, int M, int N, int x);
+void out(void);
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int N, M;
+	cin >> M >> N;
+	for (int i = 1; i <= M - N + 1; i++) {
+		v.push_back(i);
+		//cout << i << " ";
+		NM(i, M, N, 1);
+		v.pop_back();
+	}
+}
+void NM(int before, int M, int N, int x) {
+	if (x == N) {
+		if (before > M)return;
+		out();
+		cout << "\n";
+		return;
+	}
+	for (int i = before+1; i <= M - (N - before -1 ); i++) {
+		v.push_back(i);
+		//cout << i << " ";
+		NM(i, M, N, x+1);
+		v.pop_back();
+	}
+}
+void out(void) {
+	for (int i = 0; i < v.size(); i++)cout << v[i] << " ";
+}
