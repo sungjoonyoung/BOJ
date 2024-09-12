45c700760bc56fd7a14fcd905e531750c3b056c3
Joonyoung Sung <sungso376@gmail.com>
2024-09-12 13:48:33 +0900

    20240912

--- 1991.cpp
+++ 1991.cpp
@@ -1,47 +1,42 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 char arr[26][2];
-vector<char> v;
 void front(int x);
 void middle(int x);
 void back(int x);
-void outf(void);
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+	for (int i = 0; i < 26;i++)for (int j = 0; j < 2; j++)arr[i][j] = '.';
+	//cout << int('A');//65
 	int N;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
-		//cout << int('A');//65
-		char root;
-		cin >> root;
-		cin >> arr[root - 65][0] >> arr[root - 65][1];
+		char in;
+		cin >> in;
+		cin >> arr[int(in) - 65][0] >> arr[int(in) - 65][1];
 	}
 	front(0);
-	outf();
-	v.clear();
+	cout << "\n";
 	middle(0);
-	outf();
-	v.clear();
+	cout << "\n";
 	back(0);
-	outf();
-	v.clear();
+
 }
 void front(int x) {
-	v.push_back(x + 65);
-	if (arr[x][0] == '.' and arr[x][1] == '.')return;
-	if (arr[x][0] != '.')front(arr[x][0]);
-	if (arr[x][1] != '.')front(arr[x][1]);
+	cout << char(x + 65);
+	if (arr[x][0] != '.')front(int(arr[x][0]) - 65);
+	if (arr[x][1] != '.')front(int(arr[x][1]) - 65);
 }
 void middle(int x) {
-
+	if (arr[x][0] != '.')middle(int(arr[x][0]) - 65);
+	cout << char(x + 65);
+	if (arr[x][1] != '.')middle(int(arr[x][1]) - 65);
 }
 void back(int x) {
+	if (arr[x][0] != '.')back(int(arr[x][0]) - 65);
+	if (arr[x][1] != '.')back(int(arr[x][1]) - 65);
+	cout << char(x + 65);
 
-}
-void outf(void) {
-	for (int i = 0; i < v.size(); i++)cout << v[i];
-	cout << "\n";
 }
