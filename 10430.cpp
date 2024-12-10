3fe8d73187cfbb1369178c1958733036f17b1f06
Joonyoung Sung <sungso376@gmail.com>
2024-12-10 10:11:56 +0900

    20241210

---
+++ 10430.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int A, B, C;
+	cin >> A >> B >> C;
+	cout << (A + B) % C << "\n";
+	cout << (A + B) % C << "\n";
+	cout << (A * B) % C << "\n";
+	cout << (A * B) % C << "\n";
+}
