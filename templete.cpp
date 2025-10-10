dfb16d7a43a06c52a59733b7d2554f03fb900d87
Joonyoung Sung <sungso376@gmail.com>
2025-10-10 17:48:50 +0900

    ttt

---
+++ templete.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<utility>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define all(v) (v).begin(), (v).end()
+using namespace std;
+
+void solve(void){
+    
+}
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T=1;
+    // cin>>T;
+    while(T--)solve();
+}
