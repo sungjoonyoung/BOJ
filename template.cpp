62091d25b62c4778ebbb6361184570bd8d64fd4c
Joonyoung Sung Desktop <sungso376@gmail.com>
2025-11-06 20:05:39 +0900

    template

--- template.cpp
+++ template.cpp
@@ -2,6 +2,7 @@
 #include<utility>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 #include<queue>
 #define ll long long
 #define pii pair<int,int>
@@ -9,15 +10,17 @@
 #define vi vector<int>
 #define vl vector<ll>
 #define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
 using namespace std;
+void solve(void) {
 
-void solve(void){
-    
 }
-int main(void){
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int T=1;
-    // cin>>T;
-    while(T--)solve();
+    int T = 1;
+    cin >> T;
+    while (T--)solve();
 }
